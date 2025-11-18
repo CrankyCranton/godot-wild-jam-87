deecaf395410f0b830b9daba764bb905d00576ad
CrankyCranton <crantonbogsmorehooversil@gmail.com>
2025-11-18 04:10:54 -0500

    Test larger amounts of wolves

--- kinematic/player/player.gd
+++ kinematic/player/player.gd
@@ -7,6 +7,7 @@ class_name Player extends Kinematic
 var dash_cooling := false
 var has_axe := false
 var has_candle := false
+var attacking := false
 
 @onready var animation_tree: AnimationTree = $AnimationTree
 @onready var playback: AnimationNodeStateMachinePlayback = animation_tree.get(
@@ -22,8 +23,9 @@ var has_candle := false
 
 func _input(event: InputEvent) -> void:
 	if event.is_action_pressed(&"attack") and can_move:
-		velocity = Vector2()
-		can_move = false
+		#velocity = Vector2()
+		attacking = true
+		#can_move = false
 		playback.travel(&"Attack")
 	#if event.is_action_pressed(&"run") and not dash_cooling:
 		#dash_cooling = true
@@ -39,14 +41,15 @@ func physics_process(_delta: float) -> void:
 	var running := Input.is_action_pressed(&"run")
 	var input := Input.get_vector(&"left", &"right", &"up", &"down")
 	target_velocity = input * (run_speed if running else speed)
-	if input != Vector2():
-		playback.travel(&"Run" if running else &"Walk")
-		animation_tree.set(&"parameters/Idle/blend_position", input)
-		animation_tree.set(&"parameters/Walk/blend_position", input)
-		animation_tree.set(&"parameters/Run/blend_position", input)
-		animation_tree.set(&"parameters/Attack/blend_position", input)
-	else:
-		playback.travel(&"Idle")
+	if not attacking:
+		if input != Vector2():
+			playback.travel(&"Run" if running else &"Walk")
+			animation_tree.set(&"parameters/Idle/blend_position", input)
+			animation_tree.set(&"parameters/Walk/blend_position", input)
+			animation_tree.set(&"parameters/Run/blend_position", input)
+			animation_tree.set(&"parameters/Attack/blend_position", input)
+		else:
+			playback.travel(&"Idle")
 
 
 func _on_hit_box_hurt(damage: int, source: HurtBox) -> void:
@@ -86,6 +89,7 @@ func increase_max_health(bonus: int) -> void:
 func _on_animation_tree_animation_finished(anim_name: StringName) -> void:
 	if anim_name.contains("attack"):
 		can_move = true
+		attacking = false
 
 
 func _on_animation_tree_animation_started(anim_name: StringName) -> void:
